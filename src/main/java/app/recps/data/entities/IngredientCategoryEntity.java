2bc9c000afacfb8a58fa0c84685aaa96435cfad3
Oliver Dozsa <dozsa.oliver@gmail.com>
2026-04-16 17:01:25 +0200

    Added ingredient category related entities.

---
+++ src/main/java/app/recps/data/entities/IngredientCategoryEntity.java
@@ -0,0 +1,28 @@
+package app.recps.data.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "ingredient_category")
+public class IngredientCategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long id;
+
+    @OneToMany(mappedBy = "category")
+    public List<IngredientCategoryNameEntity> names;
+
+    @ManyToOne
+    public UserEntity user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "categories_ingredients",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "ingredient_id")
+    )
+    public List<IngredientEntity> ingredients;
+}
